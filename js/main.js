c35f73a579192ee1126d40c1d41f13350acd2477
Ethan Hayon <ehayon@gmail.com>
2012-02-05 14:45:05 -0500

    added cycling project bar

--- js/main.js
+++ js/main.js
@@ -9,10 +9,40 @@ $(document).ready(function() {
 		$("#header-logo #tagline").removeClass('active');		
 	});
 	// hide all of the projects except the first
-	function showNextProject(previous_project, current_project) {
+	$(".project").each(function() {
+		$(this).hide();
+	});
+	var current_project = 0;
+	var previous_project = -1;
+	showProject(previous_project, current_project);
+	function showProject(previous_project, current_project) {
+		$(".project").each(function(e) {
+			if(e != current_project)
+				$(this).hide();
+		});
+		if(previous_project == -1) {
+			// the page just loaded, don't slide anything, just place the first project
+			$(".project:eq(0)").show();
+		} else {	
+			$(".project:eq("+previous_project+")").hide('slide', {direction: 'left'}, 1000);
+			$(".project:eq("+current_project+")").show('slide', {direction: 'right'}, 1000);
+		}
 	}
+
 	setInterval(function() {	
-		
-	}, 2000);
+		previous_project = current_project;
+		current_project += 1;
+		projects_count = $(".project").length;
+		if(current_project >= (projects_count)) {
+			current_project = 0;
+		}
+		showProject(previous_project, current_project);
+	}, 5000);
+	
+	$("#link-indicator").show()	
+	//alert($("#main-navigation a.active").width());
+	$("#link-indicator").css("left",$("#main-navigation a.active").offset().left + ($("#main-navigation a.active").width() - 34));
+	$("#link-indicator").css("top",$("#main-navigation a.active").offset().top+33);
+	
 });
 
